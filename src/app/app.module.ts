0b1d3f895b63bf122d5ed97f995f288338990500
mazid <mazidmailbox@gmail.com>
2020-02-16 21:05:00 +0600

    AppMaterial module created for Mat modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { QuillModule } from 'ngx-quill';
+import { AppMaterialModule } from './app-material/app-material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TextEditorComponent } from './components/text-editor/text-editor.component';
 import { PostDetailComponent } from './components/post-detail/post-detail.component';
 
-import { MatButtonModule } from '@angular/material/button';
 import { HomeComponent } from './components/home/home.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
@@ -31,11 +32,12 @@ import { CategoryListComponent } from './components/category-list/category-list.
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
     AppRoutingModule,
     FlexLayoutModule,
     FontAwesomeModule,
-    QuillModule.forRoot(),
-    MatButtonModule
+    QuillModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
