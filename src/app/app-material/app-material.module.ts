19f3c1d71988931c406c0b8aeb3d3238904a7171
mazid <mazidmailbox@gmail.com>
2020-02-17 01:21:36 +0600

    Navigation bar style updated

--- src/app/app-material/app-material.module.ts
+++ src/app/app-material/app-material.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 
 @NgModule({
   exports: [
-    MatButtonModule
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule
   ]
 })
 export class AppMaterialModule {
