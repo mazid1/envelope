66b52de62172c22d37a1161a6a95aea838d6d834
mazid <mazidmailbox@gmail.com>
2020-02-16 17:55:45 +0600

    Routing is working fine

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
+import { TextEditorComponent } from './components/text-editor/text-editor.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'blog', component: BlogComponent },
+  { path: 'about', component: AboutMeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'post/:id', component: PostDetailComponent },
+  { path: 'edit-post/:id', component: TextEditorComponent },
+  { path: 'edit-post', component: TextEditorComponent },
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
