ab87928c2119f940dc10baadf3713521fcfa39f0
mazid <mazidmailbox@gmail.com>
2020-02-17 15:04:19 +0600

    Post card sample added

--- src/app/app-material/app-material.module.ts
+++ src/app/app-material/app-material.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
   exports: [
     MatButtonModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ]
 })
 export class AppMaterialModule {
