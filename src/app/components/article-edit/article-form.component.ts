c1676e044ad9b8695855e248f1fcf6f9d0c361e0
mazid <mazidmailbox@gmail.com>
2020-03-15 02:15:56 +0600

    wip: article form

---
+++ src/app/components/article-edit/article-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-form',
+  templateUrl: './article-form.component.html',
+  styleUrls: ['./article-form.component.css']
+})
+export class ArticleFormComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/models/article.model.ts
@@ -0,0 +1,6 @@
+export class Article {
+  title: String;
+  content: Array<any>;
+  publishedDate: Date;
+  tags: Array<String>;
+}
