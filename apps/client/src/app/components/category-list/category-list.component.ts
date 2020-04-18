e325ab47ce654a8be953c2304f69fcf2ba246e13
mazid <mazidmailbox@gmail.com>
2020-04-19 03:09:43 +0600

    wip: category list displayed at right sidebar
      - search articles by  category feature needed to be implemented

--- apps/client/src/app/components/category-list/category-list.component.ts
+++ apps/client/src/app/components/category-list/category-list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Category } from '../../models/category.model';
+import { CategoryService } from '../../services/category.service';
+
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
 export class CategoryListComponent implements OnInit {
+  categories: Category[];
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit() {
+    this.categoryService.getCategories().subscribe(data => (this.categories = data));
   }
 
+  onClick(category: string) {
+    console.log(category);
+    // todo: Search articles by category
+  }
 }
