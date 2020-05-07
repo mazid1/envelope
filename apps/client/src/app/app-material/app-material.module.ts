4ad80a9553c8d969a4b67f2ebbfce4d07a5bfa88
mazid <mazidmailbox@gmail.com>
2020-05-08 03:07:36 +0600

    wip: layout

--- apps/client/src/app/app-material/app-material.module.ts
+++ apps/client/src/app/app-material/app-material.module.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatPaginatorModule } from '@angular/material/paginator';
@@ -26,6 +27,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatDividerModule,
     MatPaginatorModule,
     MatToolbarModule,
+    MatIconModule,
     NgxMatSelectSearchModule
   ]
 })
--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {
     faFacebook, faGithub, faLinkedinIn, faTwitter, faYoutube
 } from '@fortawesome/free-brands-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,7 @@ import {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'envelope-client';
+  title = 'Envelope';
   leftSideNavOpened = true;
   rightSideNavOpened = true;
 
@@ -18,4 +19,9 @@ export class AppComponent {
   faLinkedinIn = faLinkedinIn;
   faGithub = faGithub;
   faTwitter = faTwitter;
+  faBars = faBars;
+
+  toggleLeftSidenav() {
+    this.leftSideNavOpened = !this.leftSideNavOpened;
+  }
 }
