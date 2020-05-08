edd1b399ef577e562a3998add06c856c92deb9e9
mazid <mazidmailbox@gmail.com>
2020-05-08 15:44:02 +0600

    observe breakpoints programatically

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Component, OnDestroy } from '@angular/core';
 import {
     faFacebook, faGithub, faLinkedinIn, faTwitter, faYoutube
 } from '@fortawesome/free-brands-svg-icons';
@@ -9,7 +12,7 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'Envelope';
   leftSideNavOpened = true;
   rightSideNavOpened = true;
@@ -21,7 +24,48 @@ export class AppComponent {
   faTwitter = faTwitter;
   faBars = faBars;
 
+  gtSmSub: Subscription;
+  smSub: Subscription;
+  xsSub: Subscription;
+
+  constructor(breakpointObserver: BreakpointObserver) {
+
+    this.gtSmSub = breakpointObserver.observe([
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge
+    ]).subscribe((result: BreakpointState) => {
+      if (result.matches) {
+        console.log(result, 'gt-sm');
+      }
+    });
+
+    this.smSub = breakpointObserver.observe([
+      Breakpoints.Small
+    ]).subscribe((result: BreakpointState) => {
+      if (result.matches) {
+        console.log(result, 'sm');
+      }
+    });
+
+    this.xsSub = breakpointObserver.observe([
+      Breakpoints.XSmall
+    ]).subscribe((result: BreakpointState) => {
+      if (result.matches) {
+        console.log(result, 'xs');
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.gtSmSub.unsubscribe();
+    this.smSub.unsubscribe();
+    this.xsSub.unsubscribe();
+  }
+
   toggleLeftSidenav() {
     this.leftSideNavOpened = !this.leftSideNavOpened;
   }
+
+  loadMobileContent() { }
 }
