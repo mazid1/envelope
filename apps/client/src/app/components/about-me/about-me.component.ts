fcd3e72acdc66f05349f9fb1af366575ea60c901
mazid <mazidmailbox@gmail.com>
2020-06-02 00:45:14 +0600

    Personal info section added

--- apps/client/src/app/components/about-me/about-me.component.ts
+++ apps/client/src/app/components/about-me/about-me.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
 
+  personalInfo = {
+    name: 'Mohammad Mazedul Islam',
+    title: 'Software Engineer'
+  };
+
   timelineData = [
     {
       type: 'position',
